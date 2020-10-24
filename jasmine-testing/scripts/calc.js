d56e9e9ee38ef2374220b8d2a7e9abb83cca6c65
paulday966 <paulday360@outlook.com>
2020-10-24 15:15:02 +0000

    Changed the function to run an if else loop

--- jasmine-testing/scripts/calc.js
+++ jasmine-testing/scripts/calc.js
@@ -1,6 +1,10 @@
-function addition(numOne, numTwo) {
-    if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
-        return numOne + numTwo;
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {
+        this.value += number;
     } else {
         alert("Error!");
     }
